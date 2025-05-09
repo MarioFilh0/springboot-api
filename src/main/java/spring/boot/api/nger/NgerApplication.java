c329a6527e88ca31117241251da5446a3da11f29
mario <hirumiza@hotmail.com>
2025-05-09 19:33:48 -0400

    first commit

---
+++ src/main/java/spring/boot/api/nger/NgerApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.api.nger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/api/nger/NgerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.api.nger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
