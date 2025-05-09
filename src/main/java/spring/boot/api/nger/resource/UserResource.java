a79a5b7fe68aa417369e4f2c28cc6c48c38abdb5
mario <hirumiza@hotmail.com>
2025-05-09 19:49:05 -0400

    user created

---
+++ src/main/java/spring/boot/api/nger/resource/UserResource.java
@@ -0,0 +1,27 @@
+package spring.boot.api.nger.resource;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "tb_user")
+public class UserResource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+}
