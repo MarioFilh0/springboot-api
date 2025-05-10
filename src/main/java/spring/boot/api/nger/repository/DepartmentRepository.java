32968cfa5fb5a9bad33ac35316fb9e04e55cd10d
mario <hirumiza@hotmail.com>
2025-05-09 20:11:51 -0400

    repository created

---
+++ src/main/java/spring/boot/api/nger/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package spring.boot.api.nger.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import spring.boot.api.nger.resource.DepartmentResource;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Long, DepartmentResource> {
+}
---
+++ src/main/java/spring/boot/api/nger/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package spring.boot.api.nger.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import spring.boot.api.nger.resource.UserResource;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, UserResource> {
+
+}
